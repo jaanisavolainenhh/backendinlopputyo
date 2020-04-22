6e90d18db297f2f548ee1fd5bf63c4f95ae0bae7
jaani <bgh273@myy.haaga-helia.fi>
2020-04-22 10:40:18 +0300

    locale

--- src/main/java/jaanin/projekti/BackendinLopputyo/BackendinLopputyoApplication.java
+++ src/main/java/jaanin/projekti/BackendinLopputyo/BackendinLopputyoApplication.java
@@ -1,18 +1,11 @@
 package jaanin.projekti.BackendinLopputyo;
 
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import jaanin.projekti.BackendinLopputyo.domain.Asiakas;
 import jaanin.projekti.BackendinLopputyo.domain.AsiakasRepository;
